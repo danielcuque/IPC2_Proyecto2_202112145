2ffdc6762636cff71bd676fb385b8f7b8c972fe2
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2022-09-26 17:11:57 -0600

    first commit

---
+++ controller/base/NodeForSimplyList.py
@@ -0,0 +1,14 @@
+
+class NodeForSimplyList:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return str(self.data)
+
+    def get_next(self):
+        return self.next
+    
+    def set_next(self, next):
+        self.next = next
